6201ba34de6b6cefb3d578b35fb87e788644b4c9
Ruslan <otmena163@gmail.com>
2025-06-28 18:19:15 +0400

    feat: обновлен About.jsx и Header.module.scss

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,65 +1,11 @@
-import React, { useCallback } from "react";
-import Particles from "@tsparticles/react";
-import { loadFull } from "tsparticles";
-
 import cl from "./About.module.scss";
 import Button from "../../shared/ui/button/Button";
+import ParticlesComponent from "../particles";
 
 export default function About() {
-	const particlesInit = useCallback(async (engine) => {
-		await loadFull(engine);
-	}, []);
-
 	return (
 		<div className={cl.about}>
-			<Particles
-				id='tsparticles'
-				init={particlesInit}
-				options={{
-					fullScreen: { enable: false },
-					particles: {
-						number: { value: 80, density: { enable: true, area: 800 } },
-						color: { value: "#ffffff" },
-						shape: { type: "circle" },
-						opacity: { value: 0.5, random: true },
-						size: { value: 3, random: true },
-						links: {
-							enable: true,
-							distance: 150,
-							color: "#ffffff",
-							opacity: 0.4,
-							width: 1,
-						},
-						move: {
-							enable: true,
-							speed: 2,
-							direction: "none",
-							random: true,
-							straight: false,
-							outModes: { default: "out" },
-						},
-					},
-					interactivity: {
-						events: {
-							onHover: { enable: true, mode: "repulse" },
-							onClick: { enable: true, mode: "push" },
-						},
-						modes: {
-							repulse: { distance: 100, duration: 0.4 },
-							push: { quantity: 4 },
-						},
-					},
-					detectRetina: true,
-				}}
-				style={{
-					position: "absolute",
-					top: 0,
-					left: 0,
-					width: "100%",
-					height: "100%",
-					zIndex: 0,
-				}}
-			/>
+			<ParticlesComponent style={{ position: "absolute", zIndex: "0" }} id='particles' />
 
 			<div className={cl.content}>
 				<h1 className={cl.title}>
